67dce3c9e02882832f5e26e680b8589683a4283f
MichaelXiao <y19938222@gmail.com>
2017-03-20 19:26:45 +0800

    will add podfile

---
+++ MotorHelper/MotorHelper/GetSOAPInfo.swift
@@ -0,0 +1,12 @@
+//
+//  getSOAPInfo.swift
+//  MotorHelper
+//
+//  Created by 孟軒蕭 on 20/03/2017.
+//  Copyright © 2017 MichaelXiao. All rights reserved.
+//
+import UIKit
+
+class GetSOAPInfo {
+
+}
