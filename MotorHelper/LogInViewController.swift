3ccdf58a28fc358114e280ba37cba2a85dd92f2d
MichaelXiao <y19938222@gmail.com>
2017-03-27 14:28:58 +0800

    establish a fake login page

--- MotorHelper/LogInViewController.swift
+++ MotorHelper/LogInViewController.swift
@@ -10,7 +10,24 @@ import UIKit
 import Firebase
 
 class LogInViewController: UIViewController {
+    let mail = "y19938222@gmail.com"
+    let pwd = "22871230"
 
+    @IBAction func logIn(_ sender: Any) {
+        FIRAuth.auth()?.signIn(withEmail: mail, password: pwd, completion: { (_, error) in
+            if error == nil {
+                print("登入成功")
+            } else {
+                // 提示用戶從 firebase 返回了一個錯誤。
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+
+                self.present(alertController, animated: true, completion: nil)
+            }
+        })
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
