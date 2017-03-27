9cfc83038ed9893b50c1262656371237293ae942
MichaelXiao <y19938222@gmail.com>
2017-03-27 14:15:16 +0800

    add firebase Pod

--- MotorHelper/Pods/Target Support Files/Pods-MotorHelper/Pods-MotorHelper-frameworks.sh
+++ MotorHelper/Pods/Target Support Files/Pods-MotorHelper/Pods-MotorHelper-frameworks.sh
@@ -91,12 +91,16 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AEXML/AEXML.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SWXMLHash/SWXMLHash.framework"
   install_framework "$BUILT_PRODUCTS_DIR/StringExtensionHTML/StringExtensionHTML.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AEXML/AEXML.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SWXMLHash/SWXMLHash.framework"
   install_framework "$BUILT_PRODUCTS_DIR/StringExtensionHTML/StringExtensionHTML.framework"
 fi
