435ba5368bf98ff7246a61ae275decc9a0aa8269
MichaelXiao <y19938222@gmail.com>
2017-03-27 14:05:55 +0800

    ready to add firebase Pod

---
+++ MotorHelper/LogInViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LogInViewController.swift
+//  MotorHelper
+//
+//  Created by 孟軒蕭 on 27/03/2017.
+//  Copyright © 2017 MichaelXiao. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
